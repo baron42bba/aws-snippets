8eb1be49da5e57c65ea5f50cd1aa5a6161eb3968
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-02-04 13:58:58 +0100

    fixing Steve Purcell recommendations
    - Don't use constants if used in just one place.
    - Remove unnecessary files.
    - Use better lisp style.
    
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- aws-snippets.el
+++ aws-snippets.el
@@ -30,12 +30,10 @@
 
 ;;; Code:
 
-(defvar aws-snippets-dir (file-name-directory (or (buffer-file-name)
-                                                 load-file-name)))
-(defconst aws-snippets--default-regions
-  '("us-east-1" "eu-west-1" "ap-southeast-1"))
+(defvar aws-snippets-dir (file-name-directory (or load-file-name
+						  (buffer-file-name))))
 
-(defcustom aws-snippets-regions aws-snippets--default-regions
+(defcustom aws-snippets-regions '("us-east-1" "eu-west-1" "ap-southeast-1")
   "List of AWS regions for selections."
   :tag "regions"
   :type '(choice (string :tag "Region")
@@ -46,13 +44,9 @@
                            (symbol-value symbol))))
              (set-default symbol new)
              ))
-  :group 'aws-snippets
-  )
+  :group 'aws-snippets)
 
-(defconst aws-snippets--default-profiles
-  '("test" "prod"))
-
-(defcustom aws-snippets-profiles aws-snippets--default-profiles
+(defcustom aws-snippets-profiles '("test" "prod")
   "List of AWS profiles for selections."
   :tag "profiles"
   :type '(choice (string :tag "Profile")
@@ -63,13 +57,9 @@
                            (symbol-value symbol))))
              (set-default symbol new)
              ))
-  :group 'aws-snippets
-  )
+  :group 'aws-snippets)
 
-(defconst aws-snippets--default-filters
-  '("tag:Name" "tag:Project" "tag:Type" "tag:Cluster" "instance.group-id" "ip-address" "private-ip-address" "network-interface.subnet-id" "description" "name" "iam-instance-profile.arn"))
-
-(defcustom aws-snippets-filters aws-snippets--default-filters
+(defcustom aws-snippets-filters '("tag:Name" "tag:Project" "tag:Type" "tag:Cluster" "instance.group-id" "ip-address" "private-ip-address" "network-interface.subnet-id" "description" "name" "iam-instance-profile.arn")
   "List of AWS filters for selections."
   :tag "default-filters"
   :type '(choice (string :tag "Filter")
@@ -80,188 +70,115 @@
                            (symbol-value symbol))))
              (set-default symbol new)
              ))
-  :group 'aws-snippets
-  )
+  :group 'aws-snippets)
 
-(defconst aws-snippets--ami-ls-query
-  '"reverse(sort_by(Images, &CreationDate)[].{CreationDate:CreationDate,Name:Name,ImageId:ImageId,Public:Public,Description:Description})")
-
-(defcustom aws-snippets-ami-ls-query aws-snippets--ami-ls-query
+(defcustom aws-snippets-ami-ls-query '"reverse(sort_by(Images, &CreationDate)[].{CreationDate:CreationDate,Name:Name,ImageId:ImageId,Public:Public,Description:Description})"
   "Query string used for ami-ls."
   :tag "ami-ls-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--autoscaling-describe-auto-scaling-groups-query
-  '"AutoScalingGroups[].{Name: AutoScalingGroupName, Desired: DesiredCapacity,Min: MinSize, Max: MaxSize}")
-
-(defcustom aws-snippets-autoscaling-describe-auto-scaling-groups-query aws-snippets--autoscaling-describe-auto-scaling-groups-query
+(defcustom aws-snippets-autoscaling-describe-auto-scaling-groups-query '"AutoScalingGroups[].{Name: AutoScalingGroupName, Desired: DesiredCapacity,Min: MinSize, Max: MaxSize}"
   "Query string used for autoscaling-describe-auto-scaling-groups."
   :tag "autoscaling-describe-auto-scaling-groups"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--autoscaling-describe-launch-configurations-query
-  '"reverse(sort_by(LaunchConfigurations &CreatedTime)[*].[LaunchConfigurationName, CreatedTime])")
-
-(defcustom aws-snippets-autoscaling-describe-launch-configurations-query aws-snippets--autoscaling-describe-launch-configurations-query
+(defcustom aws-snippets-autoscaling-describe-launch-configurations-query '"reverse(sort_by(LaunchConfigurations &CreatedTime)[*].[LaunchConfigurationName, CreatedTime])"
   "Query string used for autoscaling-describe-launch-configurations."
   :tag "autoscaling-describe-launch-configurations-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--ec2-get-dns-names-query
-  '"Reservations[].Instances[?State.Name=='running'].[InstanceId, PublicDnsName]")
-
-(defcustom aws-snippets-ec2-get-dns-names-query aws-snippets--ec2-get-dns-names-query
+(defcustom aws-snippets-ec2-get-dns-names-query '"Reservations[].Instances[?State.Name=='running'].[InstanceId, PublicDnsName]"
   "Query string used for aws-ec2-get-dns-names."
   :tag "ec2-get-dns-names-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--ec2-get-instance-volumes-query
-  '"Reservations[].Instances[].[join(\`,\`,Tags[?Key==\`Name\`].Value),join(' ', BlockDeviceMappings[].Ebs.VolumeId)]")
-
-(defcustom aws-snippets-ec2-get-instance-volumes-query aws-snippets--ec2-get-instance-volumes-query
+(defcustom aws-snippets-ec2-get-instance-volumes-query '"Reservations[].Instances[].[join(\`,\`,Tags[?Key==\`Name\`].Value),join(' ', BlockDeviceMappings[].Ebs.VolumeId)]"
   "Query string used for ec2-get-instance-volumes."
   :tag "ec2-get-instance-volumes-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--ec2-get-securitygroups-query
-  '"sort_by(SecurityGroups, &GroupName)[*].[GroupName, GroupId, Description]")
-
-(defcustom aws-snippets-ec2-get-securitygroups-query aws-snippets--ec2-get-securitygroups-query
+(defcustom aws-snippets-ec2-get-securitygroups-query '"sort_by(SecurityGroups, &GroupName)[*].[GroupName, GroupId, Description]"
   "Query string used for ec2-get-securitygroups."
   :tag "ec2-get-securitygroups-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--ec2-get-snapshots-query
-  '"Snapshots[].[join(\`,\`,Tags[?Key==\`Name\`].Value),StartTime,Description,SnapshotId,VolumeId,VolumeSize,Progress]")
-
-(defcustom aws-snippets-ec2-get-snapshots-query aws-snippets--ec2-get-snapshots-query
+(defcustom aws-snippets-ec2-get-snapshots-query '"Snapshots[].[join(\`,\`,Tags[?Key==\`Name\`].Value),StartTime,Description,SnapshotId,VolumeId,VolumeSize,Progress]"
   "Query string used for ec2-get-snapshots-query."
   :tag "ec2-get-snapshots-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--ec2-get-volumes-query
-  '"Volumes[].[VolumeId,State,SnapshotId,join(\`,\`,Tags[?Key==\`Name\`].Value)]")
-
-(defcustom aws-snippets-ec2-get-volumes-query aws-snippets--ec2-get-volumes-query
+(defcustom aws-snippets-ec2-get-volumes-query '"Volumes[].[VolumeId,State,SnapshotId,join(\`,\`,Tags[?Key==\`Name\`].Value)]"
   "Query string used for ec2-get-volumes-query."
   :tag "ec2-get-volumes-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--ec2-get-volumes-unused-query
-  '"Volumes[].[VolumeId,Size,VolumeType,CreateTime,State,SnapshotId]")
-
-(defcustom aws-snippets-ec2-get-volumes-unused-query aws-snippets--ec2-get-volumes-unused-query
+(defcustom aws-snippets-ec2-get-volumes-unused-query '"Volumes[].[VolumeId,Size,VolumeType,CreateTime,State,SnapshotId]"
   "Query string used for ec2-get-volumes-unused-query."
   :tag "ec2-get-volumes-unused-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--ec2-instance-running-query
-  '"Reservations[].Instances[].[InstanceId, State.Name]")
-
-(defcustom aws-snippets-ec2-instance-running-query aws-snippets--ec2-instance-running-query
+(defcustom aws-snippets-ec2-instance-running-query '"Reservations[].Instances[].[InstanceId, State.Name]"
   "Query string used for ec2-instance-running."
   :tag "ec2-instance-running-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--ec2-list-instances-query
-  '"Reservations[].Instances[].[Tags[?Key==\`Name\`].Value[] | [0],Tags[?Key==\`Schedule\`].Value[] | [0],InstanceId, State.Name, PublicDnsName, InstanceType,Placement.AvailabilityZone,LaunchTime]")
-
-(defcustom aws-snippets-ec2-list-instances-query aws-snippets--ec2-list-instances-query
+(defcustom aws-snippets-ec2-list-instances-query '"Reservations[].Instances[].[Tags[?Key==\`Name\`].Value[] | [0],Tags[?Key==\`Schedule\`].Value[] | [0],InstanceId, State.Name, PublicDnsName, InstanceType,Placement.AvailabilityZone,LaunchTime]"
   "Query string used for ec2 describe-instances."
   :tag "ec2-list-instances-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--ec2-list-subnets-query
-  '"sort_by(sort_by(Subnets[].{VpcId:VpcId, SubnetId:SubnetId, State:State, AvailabilityZone:AvailabilityZone, CidrBlock:CidrBlock, AvailableIpAddressCount:AvailableIpAddressCount Tags:to_string(Tags[].[Key, Value])}, &SubnetId), &VpcId)")
-
-(defcustom aws-snippets-ec2-list-subnets-query aws-snippets--ec2-list-subnets-query
+(defcustom aws-snippets-ec2-list-subnets-query '"sort_by(sort_by(Subnets[].{VpcId:VpcId, SubnetId:SubnetId, State:State, AvailabilityZone:AvailabilityZone, CidrBlock:CidrBlock, AvailableIpAddressCount:AvailableIpAddressCount Tags:to_string(Tags[].[Key, Value])}, &SubnetId), &VpcId)"
   "Query string used for ec2-list-subnets."
   :tag "ec2-list-subnets-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--ec2-list-vpcs-query
-  '"sort_by(Vpcs[].{VpcId:VpcId, State:State, AvailabilityZone:AvailabilityZone, CidrBlock:CidrBlock, AvailableIpAddressCount:AvailableIpAddressCount Tags:to_string(Tags[].[Key, Value])}, &VpcId)")
-
-(defcustom aws-snippets-ec2-list-vpcs-query aws-snippets--ec2-list-vpcs-query
+(defcustom aws-snippets-ec2-list-vpcs-query '"sort_by(Vpcs[].{VpcId:VpcId, State:State, AvailabilityZone:AvailabilityZone, CidrBlock:CidrBlock, AvailableIpAddressCount:AvailableIpAddressCount Tags:to_string(Tags[].[Key, Value])}, &VpcId)"
   "Query string used for ec2-list-vpcs."
   :tag "ec2-list-vpcs-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--elb-get-list-query
-  '"reverse(sort_by(LoadBalancerDescriptions, &CreatedTime)[].[LoadBalancerName, CreatedTime])")
-
-(defcustom aws-snippets-elb-get-list-query aws-snippets--elb-get-list-query
+(defcustom aws-snippets-elb-get-list-query '"reverse(sort_by(LoadBalancerDescriptions, &CreatedTime)[].[LoadBalancerName, CreatedTime])"
   "Query string used for elb-get-list."
   :tag "elb-get-list-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--emr-list-clusters-query
-  '"Clusters[*].{ID: Id, NAME: Name}")
-
-(defcustom aws-snippets-emr-list-clusters-query aws-snippets--emr-list-clusters-query
+(defcustom aws-snippets-emr-list-clusters-query '"Clusters[*].{ID: Id, NAME: Name}"
   "Query string used for emr-list-clusters."
   :tag "emr-list-clusters-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--rds-describe-instances-query
-  '"DBInstances[*].[DBInstanceIdentifier,DbiResourceId, Endpoint.Address]")
-
-(defcustom aws-snippets-rds-describe-instances-query aws-snippets--rds-describe-instances-query
+(defcustom aws-snippets-rds-describe-instances-query '"DBInstances[*].[DBInstanceIdentifier,DbiResourceId, Endpoint.Address]"
   "Query string used for rds describe-db-instances."
   :tag "rds-describe-db-instances-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--route53-list-hosted-zones-query
-  '"HostedZones[].[Id, Name]")
-
-(defcustom aws-snippets-route53-list-hosted-zones-query aws-snippets--route53-list-hosted-zones-query
+(defcustom aws-snippets-route53-list-hosted-zones-query '"HostedZones[].[Id, Name]"
   "Query string used for route53 list-hosted-zones."
   :tag "route53-list-hosted-zones-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
-(defconst aws-snippets--aws-support-ls-query
-  '"reverse(sort_by(cases, &timeCreated)[].[caseId, subject, timeCreated, displayId])")
-
-(defcustom aws-snippets-aws-support-ls-query aws-snippets--aws-support-ls-query
+(defcustom aws-snippets-aws-support-ls-query '"reverse(sort_by(cases, &timeCreated)[].[caseId, subject, timeCreated, displayId])"
   "Query string used for aws support-ls-query."
   :tag "aws-support-ls-query"
   :group 'aws-snippets
-  :type 'string
-  )
+  :type 'string)
 
 ;;;###autoload
 (defun aws-snippets-initialize ()
